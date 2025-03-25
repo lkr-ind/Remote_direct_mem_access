06bb8baae722fa4e5cff1afb23ad7393c7874fb4
Lokesh <lokesh.ragta@yahoo.com>
2025-03-25 11:01:27 +0000

    Change some details in submit.sh file

--- 01_RDMA_example/submit.sh
+++ 01_RDMA_example/submit.sh
@@ -11,5 +11,5 @@ cat $PBS_NODEFILE
 module purge
 module load OpenMPI/4.1.4-NVHPC-22.7-CUDA-11.7.0
 
-nvcc -x cu -ccbin=mpicxx -arch=sm_80 01_RDMA_mnodes_cuda_mpi.cpp
-mpirun -np 2 --mca pml ucx ./a.out
+nvcc -x cu -ccbin=mpicxx -arch=sm_80 RDMA_mnodes_cuda_mpi.cpp -o rdma_mnodes_cuda_mpi
+mpirun -np 2 --mca pml ucx ./rdma_mnodes_cuda_mpi
